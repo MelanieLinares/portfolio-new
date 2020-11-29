1251ead01262dd22ea1df01bc63a486d02e480df
MelanieLinares <mnmclaughlin0@gmail.com>
2020-11-29 15:06:19 -0500

    Update post.js

--- src/js/cms-preview-templates/post.js
+++ src/js/cms-preview-templates/post.js
@@ -9,12 +9,13 @@ export default class PostPreview extends React.Component {
     return <div id="main" className="mw6 center ph3 pv4">
       <h1 className="f2 lh-title b mb3">{ entry.getIn(["data", "title"])}</h1>
       <div className="flex justify-between grey-3">
-        <p>hi{ format(entry.getIn(["data", "date"]), "ddd, MMM D, YYYY") }</p>
+        <p>hi{ format(entry.getIn(["data", "date"]), "ddd, MMM D, YYYY") } { format(entry.getIn(["data", "newdate"]), "ddd, MMM D, YYYY") }</p>
         <p>Read in x minutes</p>
       </div>
       <div className="cms mw6">
         <p>{ entry.getIn(["data", "description"]) }</p>
         { image && <img src={ image } alt={ entry.getIn(["data", "title"])} /> }
+        { image1 && <img src={ image1 } alt={ entry.getIn(["data", "title"])} /> }
         { widgetFor("body") }
       </div>
     </div>;
