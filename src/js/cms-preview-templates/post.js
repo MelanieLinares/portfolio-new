514397ad8d8cb591b1ee0c68c7cc953b8c2230a8
MelanieLinares <mnmclaughlin0@gmail.com>
2020-11-29 15:44:06 -0500

    Update post.js

--- src/js/cms-preview-templates/post.js
+++ src/js/cms-preview-templates/post.js
@@ -5,6 +5,7 @@ export default class PostPreview extends React.Component {
   render() {
     const {entry, widgetFor, getAsset} = this.props;
     let image = getAsset(entry.getIn(["data", "image"]));
+    let image1 = getAsset(entry.getIn(["data", "image1"]));
 
     return <div id="main" className="mw6 center ph3 pv4">
       <h1 className="f2 lh-title b mb3">{ entry.getIn(["data", "title"])}</h1>
